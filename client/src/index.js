d66f9cb3d1d10d5d7d270ceb69cd4b595fd11d49
Wysogota <kmmaxim69@gmail.com>
2022-06-17 18:46:13 +0300

    Set bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,8 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import App from './App';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './common/styles/customBootstrap.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
