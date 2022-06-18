3cfa5302089ea837970f76f0bb1d332925afc136
Wysogota <kmmaxim69@gmail.com>
2022-06-18 12:56:47 +0300

    Create Header & configured bootstrap colors

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,24 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import cx from 'classnames';
+import { Button, ButtonGroup, Col, Container, Nav, Navbar, NavDropdown, Row } from 'react-bootstrap';
+import {
+  BsGridFill as CatalogIcon, BsSearch as SearchIcon, BsFillQuestionCircleFill as FAQIcon,
+  BsNewspaper as NewsIcon, BsFillEnvelopeFill as ContactsIcon, BsShuffle as RandomIcon
+} from 'react-icons/bs';
 import Logo from '../Logo';
-import styles from './Header.module.scss';
 import ToggleTheme from '../ToggleTheme';
+import styles from './Header.module.scss';
 
 const Header = () => {
-
   const { isDarkTheme } = useSelector(({ themes }) => themes);
 
-  const classes = cx(
+  const headerClasses = cx(
     styles.header,
     isDarkTheme ? styles.dark_theme : styles.light_theme
   );
 
+  const [theme, setTheme] = useState("dark");
+  useEffect(() => {
+    setTheme(isDarkTheme ? "light" : "dark");
+  }, [isDarkTheme]);
+
   return (
-    <div className={classes}>
-      <Logo />
-      <ToggleTheme />
-    </div>
+    <Container fluid className={headerClasses} >
+      <Row>
+        <Col>
+          <Logo />
+        </Col>
+        <Col>
+          <Navbar variant={theme} className={styles.navbar}>
+            <Nav><Nav.Link href="#"><CatalogIcon />Catalog</Nav.Link></Nav>
+            <Nav><Nav.Link href="#"><SearchIcon />Search</Nav.Link></Nav>
+            <Nav><Nav.Link href="#"><FAQIcon />FAQ</Nav.Link></Nav>
+            <Nav>
+              <NavDropdown menuVariant={theme}>
+                <NavDropdown.Item href="#"><NewsIcon />News</NavDropdown.Item>
+                <NavDropdown.Item href="#"><ContactsIcon />Contacts</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#"><RandomIcon />Random</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar>
+        </Col>
+        <Col>
+          <ButtonGroup>
+            <Button as={Link} to="/signin" variant={"outline-" + theme}>Sign In</Button>
+            <Button as={Link} to="/signup" variant={theme}>Sign Up</Button>
+          </ButtonGroup>
+          <ToggleTheme className={styles.padding_left} />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- client/src/components/ToggleTheme/index.jsx
+++ client/src/components/ToggleTheme/index.jsx
@@ -3,20 +3,23 @@ import { bindActionCreators } from 'redux';
 import { useSelector, useDispatch } from 'react-redux';
 import * as actionCreators from '../../redux/actions/actionCreators';
 import cx from 'classnames';
-import { BsFillMoonFill as DarkTheme, BsSunFill as LightTheme } from "react-icons/bs";
-import styles from './ToggleTheme.module.scss'
+import { BsFillMoonFill as DarkTheme, BsMoon as LightTheme } from "react-icons/bs";
+import styles from './ToggleTheme.module.scss';
 
-const ToggleTheme = () => {
+const ToggleTheme = ({ className }) => {
   const { isDarkTheme } = useSelector(({ themes }) => themes);
   const { toggleTheme } = bindActionCreators(actionCreators, useDispatch());
 
   const classes = cx(
     styles.toggle_button,
-    isDarkTheme ? styles.dark_theme : styles.light_theme
+    isDarkTheme ? styles.dark_theme : styles.light_theme,
+    className
   );
 
   return (
-    <button onClick={toggleTheme} className={classes}>{isDarkTheme ? <DarkTheme /> : <LightTheme />}</button>
+    <button onClick={toggleTheme} className={classes} >
+      {isDarkTheme ? <DarkTheme /> : <LightTheme />}
+    </button>
   );
 };
 
