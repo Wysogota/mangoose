6f25e2921e44755c14b1f076b804286ec732ebca
Wysogota <kmmaxim69@gmail.com>
2022-06-22 17:32:55 +0300

    Downgrade React to 17

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './common/styles/customBootstrap.scss'
+import './common/styles/customBootstrap.scss';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
@@ -19,6 +18,7 @@ root.render(
         </PersistGate>
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
