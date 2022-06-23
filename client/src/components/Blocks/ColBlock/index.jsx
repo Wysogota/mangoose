91ba927fa3023ba26c5f8ee073e33bb60dd3406a
Wysogota <kmmaxim69@gmail.com>
2022-06-23 22:57:17 +0300

    Refactor blocks

--- client/src/components/Blocks/ColBlock/index.jsx
+++ client/src/components/Blocks/ColBlock/index.jsx
@@ -2,19 +2,25 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Col } from 'react-bootstrap';
 import cx from 'classnames';
-import styles from './ColBlock.module.scss'
+import styles from './ColBlock.module.scss';
 
-const ColBlock = ({ children }) => {
+const ColBlock = ({ className, children }) => {
   const { theme: { bgTheme } } = useSelector(({ themes }) => themes);
   const classes = cx(
-    bgTheme,
     styles.col,
-    'rounded'
+    className,
+  );
+
+  const innerClasses = cx(
+    bgTheme,
+    'p-3 rounded',
   );
 
   return (
     <Col className={classes}>
-      {children}
+      <div className={innerClasses}>
+        {children}
+      </div>
     </Col>
   );
 };
