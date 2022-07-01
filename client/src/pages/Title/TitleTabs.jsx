f65f60d5017e2fd857ec14889486b37e7e053b0d
Wysogota <kmmaxim69@gmail.com>
2022-07-01 19:11:28 +0300

    Add Tags on page

--- client/src/pages/Title/TitleTabs.jsx
+++ client/src/pages/Title/TitleTabs.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Tab, Tabs } from 'react-bootstrap';
+import Tags from '../../components/Title/Tags';
 
 const TitleTabs = (props) => {
-  const { desc } = props;
+  const { desc, tags } = props;
   const { theme: { mainColor } } = useSelector(({ themes }) => themes);
-  
+
   return (
     <Tabs justify defaultActiveKey='info' className={`nav-tabs-${mainColor}`}>
       <Tab eventKey='info' title='Information'>
-        <Tab.Content>{desc}</Tab.Content>
+        <Tab.Content>
+          <div >{desc}</div>
+          <br />
+          <Tags data={tags} />
+        </Tab.Content>
       </Tab>
       <Tab eventKey='chapters' title='Chapters'>
       </Tab>
