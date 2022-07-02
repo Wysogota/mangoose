2464d81e801ec89410e09ce6cd448b04b54add5a
Wysogota <kmmaxim69@gmail.com>
2022-07-02 19:00:50 +0300

    Finish Arts component

--- client/src/components/Title/Arts/index.jsx
+++ client/src/components/Title/Arts/index.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { bindActionCreators } from 'redux';
 import { useSelector, useDispatch } from 'react-redux';
 import * as actionCreators from '../../../redux/actions/actionCreators';
-import { Col, Container, Image, Pagination, Row, Spinner } from 'react-bootstrap';
+import { useSearchParams } from 'react-router-dom';
+import { Col, Image, Spinner } from 'react-bootstrap';
 import { isEmpty } from 'lodash';
 import cx from 'classnames';
+import MainHeader from '../../Headers/MainHeader';
+import PaginationButtons from '../../PaginationButtons';
 import styles from './Arts.module.scss';
-import Cover from '../Cover';
 
 const limit = 5;
 
@@ -22,63 +24,50 @@ const Arts = (props) => {
   const { theme: { bgAccentTheme } } = useSelector(({ themes }) => themes);
   const { getMangaCovers } = bindActionCreators(actionCreators, useDispatch());
 
+  const [searchParams] = useSearchParams();
+  const paramValue = Number.parseInt(searchParams.get('page')) - 1;
+  const [currentPage, setCurrentPage] = useState(paramValue | 0);
+
   useEffect(() => {
-    getMangaCovers(options({ mangaId: mangaId, offset: 0 }));
-  }, []);
-  if (isEmpty(covers) || isFetching) {
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <Spinner animation="border" role="status">
-              <span className="visually-hidden">Loading...</span>
-            </Spinner>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-  else {
+    getMangaCovers(options({ mangaId: mangaId, offset: limit * currentPage }));
+  }, [currentPage]);
 
-    const getPaginationButtons = () => {
-      const count = Math.ceil(covers.total / limit);
-      return new Array(count).fill(null).map((_, i) =>
-        <Pagination.Item key={i + 1}>{i + 1}</Pagination.Item>
-      );
-    };
+  const containerClasses = cx(
+    styles.arts_conatiner,
+    bgAccentTheme,
+    'rounded p-3',
+  );
 
-    const containerClasses = cx(
-      bgAccentTheme,
-      'rounded p-2',
-    );
+  const imageClasses = cx(
+    styles.image,
+    'pe-2 rounded',
+  );
 
-    return (
-      <div className={containerClasses}>
-        <Row>
-          <Col className={styles.arts_conatiner}>
-            {
-              covers.covers.map(({ id, volume, url }) =>
-                <Image key={id} src={url} alt={volume} className={'p-2 rounded ' + styles.image} />
-              )
-            }
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Pagination className='justify-content-center p-2 pt-3'>
-              <Pagination.First />
-              <Pagination.Prev />
-              {getPaginationButtons()}
-              <Pagination.Next />
-              <Pagination.Last />
-            </Pagination>
-          </Col>
-        </Row>
-      </div>
-    );
+  const contentClasses = cx(
+    styles.image_container,
+    'd-flex justify-content-center align-items-center',
+  );
 
-  }
-
+  return (
+    <Col className={containerClasses}>
+      <Col>
+        <MainHeader>Arts</MainHeader>
+        <div className={contentClasses}>{
+          (isEmpty(covers) || isFetching)
+            ? <Spinner animation='border' role='status'></Spinner>
+            : covers.covers.map(({ id, volume, url }) =>
+              <Image key={id} src={url} alt={volume} className={imageClasses} />
+            )
+        }</div>
+      </Col>
+      <Col>
+        <PaginationButtons
+          itemCount={covers.total} limit={limit}
+          currentPage={currentPage} setCurrentPage={setCurrentPage}
+        />
+      </Col>
+    </Col>
+  );
 };
 
 export default Arts;
