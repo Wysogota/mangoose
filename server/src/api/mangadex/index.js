5cb402f04816d6fb1df8c2171373d1d2635b4e64
Wysogota <kmmaxim69@gmail.com>
2022-07-03 11:24:34 +0300

    fix array options

--- server/src/api/mangadex/index.js
+++ server/src/api/mangadex/index.js
@@ -2,14 +2,14 @@ const axios = require('axios');
 const queryString = require('query-string');
 const Manga = require('../../models/Manga');
 const Cover = require('../../models/Cover');
-const { coversOptions, defaultListOptions, relationshipsOptions } = require('./options');
+const { coversOptions, defaultListOptions, relationshipsOptions, stringifyOptions } = require('./options');
 
 const client = axios.create({
   baseURL: 'https://api.mangadex.org',
 });
 
 module.exports.getMangaCovers = async (options) => {
-  const query = queryString.stringify(coversOptions(options));
+  const query = queryString.stringify(coversOptions(options), stringifyOptions);
   const { data: { data, limit, offset, total } } = await client.get(`/cover?${query}`);
   const response = {
     covers: data.map((item) => new Cover(item)),
@@ -26,14 +26,14 @@ module.exports.getMangaList = async (options) => {
     defaultListOptions,
     relationshipsOptions
   );
-  const query = queryString.stringify(assignedOptions);
+  const query = queryString.stringify(assignedOptions, stringifyOptions);
   const { data: { data } } = await client.get(`/manga?${query}`);
   const mangaList = data.map((item) => new Manga(item));
   return mangaList;
 };
 
 module.exports.getManga = async (options) => {
-  const query = queryString.stringify(relationshipsOptions);
+  const query = queryString.stringify(relationshipsOptions, stringifyOptions);
   const { data: { data } } = await client.get(`/manga/${options.mangaId}?&${query}`);
   const manga = new Manga(data);
   return manga;
--- server/src/api/mangadex/options.js
+++ server/src/api/mangadex/options.js
@@ -1,16 +1,20 @@
 module.exports.defaultListOptions = {
   includedTagsMode: 'AND',
-  'excludedTags[]': 'b13b2a48-c720-44a9-9c77-39c9979373fb', /* -- doujinshi -- */
-  'contentRating[]': ['safe', 'suggestive'],
+  'excludedTags': ['b13b2a48-c720-44a9-9c77-39c9979373fb'], /* -- doujinshi -- */
+  'contentRating': ['safe', 'suggestive'],
 };
 
 module.exports.relationshipsOptions = {
-  'includes[]': ['cover_art', 'author', 'artist']
+  'includes': ['cover_art', 'author', 'artist']
 };
 
 module.exports.coversOptions = (options) => ({
-  'manga[]': options.mangaId,
+  'manga': [options.mangaId],
   'order[volume]': 'asc',
   limit: options.limit,
   offset: options.offset,
 });
+
+module.exports.stringifyOptions = {
+  arrayFormat: 'bracket'
+};
