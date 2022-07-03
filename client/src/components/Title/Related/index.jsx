1b6ca79fe398e8036b8bc37c1fe30024100beff5
Wysogota <kmmaxim69@gmail.com>
2022-07-03 19:32:46 +0300

    Create Related tab

---
+++ client/src/components/Title/Related/index.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import { bindActionCreators } from 'redux';
+import { useSelector, useDispatch } from 'react-redux';
+import * as actionCreators from '../../../redux/actions/actionCreators';
+import { Spinner } from 'react-bootstrap';
+import MainHeader from '../../Headers/MainHeader';
+import { isEmpty } from 'lodash';
+import { selectRelationshipAttr } from '../../../common/functions';
+import RelatedCard from '../RelatedCard';
+import CONSTANTS from '../../../constants';
+import { capitalize } from 'lodash';
+
+const Related = (props) => {
+  const { relationships } = props;
+  const filteredRelationshops = relationships.filter((item) => item.related && item.related !== 'doujinshi' && item.related !== 'based_on');
+  const { mangaCatalog, isFetching } = useSelector(({ mangaCatalog }) => mangaCatalog);
+  const { getMangaCatalog } = bindActionCreators(actionCreators, useDispatch());
+  const [relatedManga, setRelatedManga] = useState([]);
+
+  useEffect(() => {
+    const mangaIds = filteredRelationshops.map(({ id }) => id);
+    getMangaCatalog({ ids: mangaIds, limit: 100 });
+  }, []);
+
+  useEffect(() => {
+    if (mangaCatalog.length === filteredRelationshops.length) {
+      const relatedList = filteredRelationshops.map(({ id, related }) => ({
+        id,
+        related: capitalize(related.replaceAll('_', ' ')),
+      }));
+
+      setRelatedManga(
+        mangaCatalog.map(t1 => ({
+          ...t1, ...relatedList.find(t2 => t2.id === t1.id)
+        }))
+      );
+    }
+  }, [mangaCatalog]);
+
+  return (
+    <div>
+      <MainHeader>Related</MainHeader>
+      {(isEmpty(relatedManga) || isFetching)
+        ? <Spinner animation='border' role='status'></Spinner>
+        : relatedManga.map(({
+          id, related, relationships,
+          attributes: {
+            title, description, status, tags
+          }
+        }) => (
+          <RelatedCard key={id}
+            id={id}
+            image={selectRelationshipAttr(relationships, 'cover_art').url}
+            related={related}
+            title={title[CONSTANTS.DEFAULT_LOCALE]}
+            description={description[CONSTANTS.DEFAULT_LOCALE]}
+            status={capitalize(status)}
+            tags={tags}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default Related;
