9bb0b3838460a15c9a6a2210c00594dff200126d
Wysogota <kmmaxim69@gmail.com>
2022-07-04 19:33:53 +0300

    selectRelationshipAttr can take array of types

--- client/src/common/functions/index.js
+++ client/src/common/functions/index.js
@@ -1,2 +1,13 @@
-export const selectRelationshipAttr = (relationships, type) =>
-  relationships.filter((item) => item.type === type)[0].attributes;
+import { isArray } from "lodash";
+
+export const selectRelationshipAttr = (relationships, type) => {
+  if (isArray(type)) {
+    const result = {};
+    type.forEach((type) => Object.assign(
+      result, {
+      [type]: relationships.filter(({ type: itemType }) => type === itemType)[0].attributes
+    }));
+    return result;
+  }
+  return relationships.filter((item) => item.type === type)[0].attributes;
+};
