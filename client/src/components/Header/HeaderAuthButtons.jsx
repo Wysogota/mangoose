4b51e7fb56b4583ac2e56a12b6c5642aba1c763e
Wysogota <kmmaxim69@gmail.com>
2022-07-23 21:33:50 +0300

    Create HeaderAuthButtons component

---
+++ client/src/components/Header/HeaderAuthButtons.jsx
@@ -0,0 +1,36 @@
+
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { useSelector, useDispatch } from 'react-redux';
+import * as actionCreators from '../../redux/actions/actionCreators';
+import { isEmpty } from 'lodash';
+import { Button, ButtonGroup } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Avatar from '../Avatar';
+import { useLoading } from '../../hooks';
+import CONSTANTS from '../../constants';
+const { PAGES: { SIGN_UP: { path: signUpPath } } } = CONSTANTS;
+
+const HeaderAuthButtons = () => {
+  const { theme: { invertedColor, outlineColor } } = useSelector(({ themes }) => themes);
+  const { me, isFetching } = useSelector(({ me }) => me);
+  const { showSignIn } = bindActionCreators(actionCreators, useDispatch());
+
+  const loading = useLoading({ isFetching, spinner: false });
+  if (loading) return loading;
+
+  return (
+    isEmpty(me) ?
+      <ButtonGroup className='pt-2 pb-2'>
+        <Button onClick={showSignIn} variant={outlineColor}>Sign In</Button>
+        <Button as={Link} to={signUpPath} variant={invertedColor}>Sign Up</Button>
+      </ButtonGroup> :
+      <span>
+        <Button variant={outlineColor}>Sign Out</Button>
+        <Avatar />
+      </span>
+
+  );
+};
+
+export default HeaderAuthButtons;
--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
-import { bindActionCreators } from 'redux';
-import { useSelector, useDispatch } from 'react-redux';
-import * as actionCreators from '../../redux/actions/actionCreators';
-import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import cx from 'classnames';
-import { Button, ButtonGroup, Col, Container, Row } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import HeaderNavbar from './HeaderNavbar';
 import HeaderSidebar from './BurgerButton';
 import Logo from '../Logo';
 import ToggleTheme from '../ToggleTheme';
 import styles from './Header.module.scss';
-import CONSTANTS from '../../constants';
-const { PAGES: { SIGN_UP: { path } } } = CONSTANTS;
+import HeaderAuthButtons from './HeaderAuthButtons';
 
 const Header = () => {
-  const { theme: { mainTheme, bgTheme, invertedColor, outlineColor } } = useSelector(({ themes }) => themes);
-  const { showSignIn } = bindActionCreators(actionCreators, useDispatch());
+  const { theme: { mainTheme, bgTheme } } = useSelector(({ themes }) => themes);
 
   const headerClasses = cx(
     styles.header,
@@ -28,13 +23,10 @@ const Header = () => {
     <Container fluid id='header' className={headerClasses}>
       <Row className='justify-content-center'>
         <Col xs='2' sm='1' className='d-md-none'> <HeaderSidebar /> </Col>
-        <Col sm='4' md='3' lg='4' className='d-none d-sm-block text-md-center'> <Logo /> </Col>
+        <Col sm='4' md='3' lg='4' className='d-none d-sm-block text-md-center'><Logo /></Col>
         <Col md='6' lg='4' className='d-none d-md-block'><HeaderNavbar /></Col>
         <Col xs='10' sm='6' md='3' lg='4' className='text-end text-md-center'>
-          <ButtonGroup className='pt-2 pb-2'>
-            <Button onClick={showSignIn} variant={outlineColor}>Sign In</Button>
-            <Button as={Link} to={path} variant={invertedColor}>Sign Up</Button>
-          </ButtonGroup>
+          <HeaderAuthButtons />
           <ToggleTheme Component='button' btnClasses='ps-3 d-none d-lg-inline-block' imageClasses='fs-3' />
         </Col>
       </Row>
