af901504f66408f2f83baf7d4ded2e1a3620eebb
Wysogota <kmmaxim69@gmail.com>
2022-07-29 13:26:05 +0300

    Create Tabs component

---
+++ client/src/components/Tabs/index.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { Tabs as BsTabs } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
+import CONSTANTS from '../../constants';
+const {
+  PARAM_NAME: { TAB }
+} = CONSTANTS;
+
+const Tabs = (props) => {
+  const { defaultTab, children } = props;
+  const { theme: { mainColor } } = useSelector(({ themes }) => themes);
+  const [searchParams] = useSearchParams();
+  const [paramValue, setParamValue] = useState(searchParams.get(TAB));
+  useEffect(() => setParamValue(searchParams.get(TAB)), [searchParams]);
+
+  return (
+    <BsTabs
+      justify
+      activeKey={paramValue || defaultTab}
+      onSelect={(key) => setParamValue(key)}
+      className={`nav-tabs-${mainColor}`}
+      data-link-tab='link-tab'
+      mountOnEnter
+    >
+      {children}
+    </BsTabs>
+  );
+};
+
+export default Tabs;
--- client/src/pages/Title/TitleTabs.jsx
+++ client/src/pages/Title/TitleTabs.jsx
@@ -1,53 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { useSearchParams } from 'react-router-dom';
-import { Col, Tab, Tabs } from 'react-bootstrap';
+import React from 'react';
+import { Col, Tab } from 'react-bootstrap';
+import Tabs from '../../components/Tabs';
 import Tags from '../../components/Title/Tags';
 import Arts from '../../components/Title/Arts';
 import Chapters from '../../components/Chapters';
 import Related from '../../components/Title/Related';
-import TabLink from '../../components/Tabs/TabLink';
 import TitleInfoList from './TitleInfoList';
+import TabLink from '../../components/Tabs/TabLink';
 import CONSTANTS from '../../constants';
-const {
-  PARAM_NAME: { TAB },
-  TITLE_TABS: { INFO, CHAPTERS, RELATED, COMMENTS }
-} = CONSTANTS;
+const { TITLE_TABS: { INFO, CHAPTERS, RELATED, COMMENTS } } = CONSTANTS;
 
 const TitleTabs = (props) => {
   const { mangaId, desc, tags, relationships, titleInfoAttr } = props;
-  const { theme: { mainColor } } = useSelector(({ themes }) => themes);
-  const [searchParams] = useSearchParams();
-  const [paramValue, setParamValue] = useState(searchParams.get(TAB));
-  useEffect(() => setParamValue(searchParams.get(TAB)), [searchParams]);
 
   return (
-    <Tabs
-      justify
-      activeKey={paramValue || INFO}
-      onSelect={(key) => setParamValue(key)}
-      className={`nav-tabs-${mainColor}`}
-      data-link-tab='link-tab'
-      mountOnEnter
-    >
+    <Tabs defaultTab={INFO}>
       <Tab eventKey={INFO} title={<TabLink to={INFO}>Information</TabLink>}>
-        <Tab.Content>
-          <Col>{desc}</Col>
-          <br />
-          <TitleInfoList attributes={titleInfoAttr} className='d-block d-lg-none' inline />
-          <br />
-          <Tags data={tags} />
-          <br />
-          <Arts mangaId={mangaId} />
-        </Tab.Content>
+        <Col>{desc}</Col>
+        <br />
+        <TitleInfoList attributes={titleInfoAttr} className='d-block d-lg-none' inline />
+        <br />
+        <Tags data={tags} />
+        <br />
+        <Arts mangaId={mangaId} />
       </Tab>
       <Tab eventKey={CHAPTERS} title={<TabLink to={CHAPTERS}>Chapters</TabLink>}>
         <Chapters mangaId={mangaId} />
       </Tab>
       <Tab eventKey={RELATED} title={<TabLink to={RELATED}>Related</TabLink>}>
-        <Tab.Content>
-          <Related relationships={relationships} />
-        </Tab.Content>
+        <Related relationships={relationships} />
       </Tab>
       <Tab eventKey={COMMENTS} title={<TabLink to={COMMENTS}>Commets</TabLink>} disabled>
       </Tab>
