712f83409291805ac12afed18ccc6d91d8571472
Wysogota <kmmaxim69@gmail.com>
2022-07-29 13:25:03 +0300

    Refactor  Volumes component

--- client/src/components/Chapters/Volumes/index.jsx
+++ client/src/components/Chapters/Volumes/index.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
 import { Accordion } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
+import cx from 'classnames';
 import VolumeChapters from './VolumeChapters';
-import styles from './Volumes.module.scss';
 
 const Volumes = (props) => {
   const { chapters } = props;
-  const { theme: { invertedColor, mainColor } } = useSelector(({ themes }) => themes);
+  const { theme: { invertedColor, bgAccentTheme } } = useSelector(({ themes }) => themes);
 
   const getVolumesList = () => [...new Set(
     chapters.map(({ attributes: { volume } }) => volume)
   )];
 
+  const classes = cx(
+    `accordion-${invertedColor}`,
+    bgAccentTheme,
+    'rounded mb-3',
+  );
+
   return (
     <section>{
       getVolumesList().map((volume) =>
-        <Accordion key={volume}
-          className={
-            `accordion-${invertedColor} rounded mb-3 `
-            + styles[`volume-${mainColor}`]
-          }
-          defaultActiveKey={volume}
-        >
+        <Accordion key={volume} className={classes} defaultActiveKey={volume}>
           <Accordion.Item eventKey={volume} >
-            <Accordion.Header className={styles.header}>Volume {volume}</Accordion.Header>
-            <Accordion.Body >{<VolumeChapters chapters={chapters} volumeName={volume} />}</Accordion.Body>
+            <Accordion.Header>Volume {volume}</Accordion.Header>
+            <Accordion.Body>
+              <VolumeChapters chapters={chapters} volumeName={volume} />
+            </Accordion.Body>
           </Accordion.Item>
         </Accordion>
       )
