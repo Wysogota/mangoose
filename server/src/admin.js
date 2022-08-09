eedf4041f8281c512b1a6be9bcaeacda817f54ec
Wysogota <kmmaxim69@gmail.com>
2022-08-09 12:22:32 +0300

    Add sequelize to admin panel

--- server/src/admin.js
+++ server/src/admin.js
@@ -1,13 +1,16 @@
 const AdminJS = require('adminjs');
 const AdminJSExpress = require('@adminjs/express');
+const AdminJSSequelize = require('@adminjs/sequelize');
 const AdminJSMongoose = require('@adminjs/mongoose');
+const sequelize = require('./models');
 const mongoose = require('mongoose');
 require('./models/mongo/MangaLists');
 
+AdminJS.registerAdapter(AdminJSSequelize);
 AdminJS.registerAdapter(AdminJSMongoose);
 
 const adminJs = new AdminJS({
-  databases: [mongoose],
+  databases: [mongoose, sequelize],
   rootPath: '/admin',
 });
 
