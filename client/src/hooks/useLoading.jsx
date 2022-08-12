4063f407f2966ac0bfebb4daded9173c2a18a523
Wysogota <kmmaxim69@gmail.com>
2022-08-12 21:28:57 +0300

    Refactor hook

--- client/src/hooks/useLoading.jsx
+++ client/src/hooks/useLoading.jsx
@@ -6,18 +6,14 @@ import MinorHeader from '../components/Headers/MinorHeader';
 const useLoading = (options) => {
   const { data, title, isFetching, spinner = true } = options;
 
-  if (!spinner && isFetching) {
-    return <></>;
-  }
-
-  if (isFetching) {
-    return (
+  if (isEmpty(data)) {
+    if (!spinner && isFetching) return (
+      <></>
+    );
+    if (isFetching) return (
       <Spinner animation='border' role='status' />
     );
-  }
-
-  if (title && isEmpty(data)) {
-    return (
+    if (title) return (
       <MinorHeader className='text-center mt-5 mb-5 fs-3'>{title}</MinorHeader>
     );
   }
