17f77d47227676511239f5d49419dd7367aceb59
Wysogota <kmmaxim69@gmail.com>
2022-08-18 15:21:19 +0300

    Add Main header overflow style

--- client/src/components/Headers/MainHeader/index.jsx
+++ client/src/components/Headers/MainHeader/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import cx from 'classnames';
+import styles from './MainHeader.module.scss';
 
-const MainHeader = ({ className, children }) => {
+const MainHeader = (props) => {
+  const { className, children } = props;
+  
   const classes = cx(
+    styles.header,
     className,
     'pb-3 m-0',
   );
-  
+
   return (
     <h3 className={classes}>{children}</h3>
   );
